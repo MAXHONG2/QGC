6db750ae0c7f4afd3607212eb92215bdfd837b6e
MAXHONG <345797998@qq.com>
2023-05-08 23:42:33 +0800

    Update sidebar.tsx

--- app/components/sidebar.tsx
+++ app/components/sidebar.tsx
@@ -96,7 +96,7 @@ export function SideBar(props: { className?: string }) {
       }`}
     >
       <div className={styles["sidebar-header"]}>
-        <div className={styles["sidebar-title"]}>ChatGPT Next</div>
+        <div className={styles["sidebar-title"]}>AIChat全过程工程咨询</div>
         <div className={styles["sidebar-sub-title"]}>
           Build your own AI assistant.
         </div>
